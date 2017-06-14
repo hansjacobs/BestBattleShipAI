5541a7ac3062a08a34d335246c6cb79212287004
Wouter Groeneveld <wouter.groeneveld@gmail.com>
2017-06-14 15:10:16 +0200

    gulp jasmine fix: coffeefiyen

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var coffee = require('gulp-coffee');
 var jasmineBrowser = require('gulp-jasmine-browser');
 
-gulp.task('jasmine', function() {
+gulp.task('coffee', function() {
+  gulp.src('./coffeescripts/*.coffee')
+    .pipe(coffee({bare: true}))
+    .pipe(gulp.dest('./javascripts/'));
+});
+
+gulp.task('jasmine', ['coffee'], () => {
   return gulp.src(['javascripts/ShipMaker.js', 'javascripts/BoardCreator.js', 'spec/*spec.js'])
     .pipe(jasmineBrowser.specRunner({console: true}))
     .pipe(jasmineBrowser.headless());
